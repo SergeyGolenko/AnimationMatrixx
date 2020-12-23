b6b7fc16d390731177537aa7289677b05a389a7b
SergeyGolenko <1989sergiotay@gmail.com>
2020-12-23 14:13:49 +0200

    done

--- AnimationMatrixx/ViewController.swift
+++ AnimationMatrixx/ViewController.swift
@@ -8,12 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var count = 0
+    
+    var timerMatrixx = Timer()
+    
+    var flag = true
+    
+    
+    @IBOutlet weak var titleButton: UIButton!
+    @IBAction func startAndStopAnimation(_ sender: Any) {
+      goAnimation()
+        
+    }
+    
+    
+    @IBOutlet weak var matrixxImage: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
-
+    
+    
+    func goAnimation(){
+        if flag {
+            timerMatrixx = Timer.scheduledTimer(timeInterval:0.1, target: self, selector: #selector(updateImage), userInfo: nil, repeats: true)
+            titleButton.setTitle("Stop", for: .normal)
+            flag = false
+            
+        } else {
+            timerMatrixx.invalidate()
+            titleButton.setTitle("Start", for: .normal)
+            flag = true
+            
+            
+        }
+        
+    }
+    
+    
+   @objc func updateImage(){
+        matrixxImage.image = UIImage(named: "frame_\(count)_delay-0.05s")
+        count += 1
+        if count == 37 {
+            count = 0
+        }
+    }
 
 }
 
